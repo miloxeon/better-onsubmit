67fd086b6e7b0cf4ba11c4cc92a63c6bdb64b385
uyouthe <melvoloskov@ya.ru>
2020-09-05 11:10:12 +0300

    cosmopolitized

---
+++ src/index.js
@@ -0,0 +1,40 @@
+export default cb => e => {
+	e.preventDefault()
+	const form = e.target
+
+	if (!form) {
+		console.warn('No form detected by formie')
+		return
+	}
+
+	const rawChildren = form.querySelectorAll('*')
+	const children = Array.prototype.slice.call(rawChildren)
+	const inputs = children.filter(node => node.type === 'radio'
+		? node.checked
+		: Boolean(node.name)
+	)
+
+	let result = {}
+
+	inputs.forEach(input => {
+		let value;
+
+		if (input.type === 'checkbox') {
+			value = input.checked
+		} else if (input.type === 'file') {
+			value = input.files
+		} else if (input.type === 'select-multiple') {
+			value = Array.prototype.slice.call(input.options).filter(
+				option => option.selected
+			).map(
+				option => option.value
+			)
+		} else {
+			value = input.value
+		}
+
+		result[input.name] = value
+	})
+
+	cb(result)
+}
